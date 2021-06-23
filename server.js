83d7df90d0f69b0fd4235bac59452ac28bae590f
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-06-23 14:26:46 +0000

    updated from web ide

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const express = require('express')
-const cors = require('cors')
-const socket = require('socket.io')
-const http = require('http')
+const express = require('express');
+const cors = require('cors');
+const socket = require('socket.io');
+const http = require('http');
 
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 const io = socket(server, {
     cors: {
         origin: ['http://localhost:3000']
     }
-})
+});
 
 function getAllChats(sender, receiver) {
-    return ['hello-world']
+    return ['hello-world', 'updated by web ide'];
 }
 
-app.use(cors())
+app.use(cors());
 
 app.get('/all-chats', (req, res) => {
     const sender = req.query['sender'];
     const receiver = req.query['receiver'];
-    res.json({ 'chats': getAllChats(sender, receiver) })
-})
+    res.json({ 'chats': getAllChats(sender, receiver) });
+});
 
 
 io.on('connection', (socket) => {
     io.allSockets().then(sockets => {
-        io.emit('contact-list', [...sockets])
+        io.emit('contact-list', [...sockets]);
         socket.on('msg-send', (msg, id) => {
             console.log(`${msg} was sent to ${id}`);
-            socket.to(id).emit('msg-receive', msg, socket.id)
-        })
+            socket.to(id).emit('msg-receive', msg, socket.id);
+        });
         socket.on('chat-leave', (id) =>{
             console.log(`a socket with id ${id} just left the ckat`);
-        })
-    })
+        });
+    });
 
 });
 
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-const port = process.env["PORT"] || 3001
+const port = process.env["PORT"] || 3001;
 
 server.listen(port, () => {
     console.log("the server is running");
-})
+});
